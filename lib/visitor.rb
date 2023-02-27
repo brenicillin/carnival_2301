2e5cf50753e4b5b7c4f4aceb8ad7405cb2c10fe6
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-02-27 11:12:46 -0500

    add initialize and format money methods

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,3 +1,17 @@
 class Visitor
-  
+  attr_reader :name,
+              :height,
+              :spending_money,
+              :preferences
+
+  def initialize(name, height, spending_money)
+    @name = name
+    @height = height
+    @spending_money = format_money(spending_money)
+    @preferences = []
+  end
+
+  def format_money(money_string)
+    money_string.gsub(/\D/,'').to_i
+  end
 end
