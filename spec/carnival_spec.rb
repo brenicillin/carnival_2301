5ba033bb0626e486127003bca68200e1b002996d
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-02-27 12:02:00 -0500

    add add_ride to carnival tests

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -18,4 +18,24 @@ RSpec.describe Carnival do
       expect(carnival.rides).to eq ([])
     end
   end
+
+  describe '#add_ride' do
+    it 'can add fully defined rides' do
+      carnival = Carnival.new(7)
+      carnival.add_ride({
+        name: 'Roller Coaster',
+        min_height: 54,
+        admission_fee: 2,
+        excitement: :thrilling
+        })
+      carnival.add_ride({
+        name: 'Carousel',
+        min_height: 24,
+        admission_fee: 1,
+        excitement: :gentle
+       })
+
+        expect(carnival.rides).to all(be_a(Ride))
+    end
+  end
 end
